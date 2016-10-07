e58d263985065b53a66f88c30ead9517a5062222
doogoo <760625325@qq.com>
2016-10-07 13:42:34 +0800

    update

---
+++ sdk/src/main/java/com/tencent/qcloud/sdk/Constant.java
@@ -0,0 +1,14 @@
+package com.tencent.qcloud.sdk;
+
+/**
+ * 常量
+ */
+public class Constant {
+
+    public static final int ACCOUNT_TYPE = 	7448;
+    //sdk appid 由腾讯分配
+    public static final int SDK_APPID = 1400014666;
+
+    public static final String BMOB_KEY ="85c5b6798e78235fe3a5e7c92a9bb713";
+
+}
